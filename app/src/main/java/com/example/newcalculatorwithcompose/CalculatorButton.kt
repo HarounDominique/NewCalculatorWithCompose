491debc5d5c2c7382271e5e9893f73418ebda29d
Haroun <haroundominique@gmail.com>
2022-11-15 23:24:59 +0100

    first commit

---
+++ app/src/main/java/com/example/newcalculatorwithcompose/CalculatorButton.kt
@@ -0,0 +1,34 @@
+package com.example.newcalculatorwithcompose
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.Symbol
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CalculatorButton(
+    symbol: String,
+    modifier: Modifier,
+    onClic: () -> Unit
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(CircleShape)
+            .clickable { onClic() }
+            .then(modifier)
+    ){
+        Text(
+            text = symbol,
+            fontSize = 36.sp,
+            color = Color.White
+        )
+    }
+}
