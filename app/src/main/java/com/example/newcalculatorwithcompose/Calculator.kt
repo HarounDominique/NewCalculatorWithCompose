b2badc1d0647a431ad5ba61dd96a265726f4b2f8
Haroun <haroundominique@gmail.com>
2022-11-16 01:09:00 +0100

    all classes written, now for real

---
+++ app/src/main/java/com/example/newcalculatorwithcompose/Calculator.kt
@@ -0,0 +1,12 @@
+package com.example.newcalculatorwithcompose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Calculator(
+    state: CalculatorState,
+    modifier: Modifier = Modifier
+) {
+    
+}
--- app/src/main/java/com/example/newcalculatorwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/newcalculatorwithcompose/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.newcalculatorwithcompose.ui.theme.NewCalculatorWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +19,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NewCalculatorWithComposeTheme {
-                val viewModel
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
             }
         }
     }
