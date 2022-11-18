0fcfc1b4e99471ea1aa7ca2390125feb732f9f12
Haroun <haroundominique@gmail.com>
2022-11-18 02:21:44 +0100

    adding functionality and the end

--- app/src/main/java/com/example/newcalculatorwithcompose/Calculator.kt
+++ app/src/main/java/com/example/newcalculatorwithcompose/Calculator.kt
@@ -34,7 +34,7 @@ fun Calculator(
             verticalArrangement = Arrangement.spacedBy(buttonSpacing)
         ) {
             Text(
-                text = state.number1 + (state.operation ?: "") + state.number2,
+                text = state.number1 + (state.operation?.symbol ?: "") + state.number2,
                 textAlign = TextAlign.End,
                 modifier = Modifier
                     .fillMaxWidth()
--- app/src/main/java/com/example/newcalculatorwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/newcalculatorwithcompose/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.newcalculatorwithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.newcalculatorwithcompose.ui.theme.MediumGray
 import com.example.newcalculatorwithcompose.ui.theme.NewCalculatorWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +26,18 @@ class MainActivity : ComponentActivity() {
                 val viewModel = viewModel<CalculatorViewModel>()
                 val state = viewModel.state
                 val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
+
+
+
             }
         }
     }
